8bd46938a35cb7fb4d2612f0071ca098e3eb87c9
sagarhani <sagarhani33@gmail.com>
2019-01-25 23:47:23 +0530

    Updates

---
+++ precache-manifest.1d3d708be7c8d5f55cac5d5a93ab5a4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07dc85584a654d097060",
+    "url": "/react-chessboard/static/js/runtime~main.07dc8558.js"
+  },
+  {
+    "revision": "8773856272a9866de2a8",
+    "url": "/react-chessboard/static/js/main.87738562.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-chessboard/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8773856272a9866de2a8",
+    "url": "/react-chessboard/static/css/main.d4b9c9a0.chunk.css"
+  },
+  {
+    "revision": "2085f3533e80ed7bc23cf7bd889e97be",
+    "url": "/react-chessboard/index.html"
+  }
+];
